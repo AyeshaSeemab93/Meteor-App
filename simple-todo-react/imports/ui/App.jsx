1152998c09f25400ac8739068bb53b572aa70038
Ayesha Seemab <ayeshaseemab2017@gmail.com>
2024-02-23 10:34:27 +0200

    hook useTracker

--- simple-todo-react/imports/ui/App.jsx
+++ simple-todo-react/imports/ui/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Task } from './Task.jsx';
+// importing hook from package(react-meteor-data) to add reactivity to the component
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '../api/TasksCollection';
 
+// const tasks = [
+//   {_id: 1, text: 'This is task 1'},
+//   {_id: 2, text: 'This is task 2'},
+//   {_id: 3, text: 'This is task 3'}
+// ]
 
-const tasks = [
-  {_id: 1, text: 'This is task 1'},
-  {_id: 2, text: 'This is task 2'},
-  {_id: 3, text: 'This is task 3'}
-]
+export const App = () => {
+  //fetching the data from the collection and passing it to the Task component
+  const tasks = useTracker(()=>{ return TasksCollection.find({}).fetch()});
 
-export const App = () => (
+  return(
   <div>
     <h1>Welcome to Meteor!</h1>
     <ul>
       {tasks.map(task => <Task key={task._id} task={task} />)}
     </ul>
   </div>
-);
+  )
+};
